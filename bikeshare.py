bad360f2cfd200bb348c988cfd047026a73d7c2c
Abdulrahman <abudrm2020@gmail.com>
2022-01-19 19:33:55 +0300

    refactor, remove unused imports

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,6 @@
 import time
 import pandas as pd
 import numpy as np
-import calendar # used in time_state
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
